983a00f9c5e7d122f2ea15e9b4ab6fc6046d4436
bkesk <8075511+bkesk@users.noreply.github.com>
2022-02-13 17:49:59 -0500

    Remove unused web resources

--- bad_apps_blog/__init__.py
+++ bad_apps_blog/__init__.py
@@ -43,11 +43,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
     # register the DBs with the current app instance
     from . import db
     db.init_app(app)
